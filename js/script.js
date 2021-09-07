82bc6a2b278f85cc1e05db120283e3d8eca0ba63
intisar eusha <iftikharrasha@gmail.com>
2021-09-07 14:24:19 +0600

    hero section responsiveness done

--- js/script.js
+++ js/script.js
@@ -14,13 +14,38 @@ var months = [
 ];
 
 window.onload = function () {
+    var fiveMinutes = 30 * 10;
+    var display = document.querySelector('#timer');
+    startExpire(fiveMinutes, display);
+
     const date = new Date();
     const month = date.getMonth();
     const day = date.getDate();
     const year = date.getFullYear();
   
-    $('.date--today').text(`${months[month]} ${day},${year}`);
+    $('.date--today').text(`${months[month]} ${day}, ${year}`);
 };
+
+function startExpire(duration, display) {
+    var timer = duration,
+    minutes,
+    seconds;
+    var timeInterval = setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? '' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    display.textContent = minutes + ':' + seconds;
+
+    if (--timer < 0) {
+      clearInterval(timeInterval);
+      startExpire(duration, display);  //timer loops again
+    //   window.location.reload(1);
+    }
+  }, 1000);
+}
 
 function startSurvey() {
     //screen #1
